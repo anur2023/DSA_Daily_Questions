68b9717c91a680266332ba69d22158dd1e3466a5
ANURUDDH KUMAR <154400979+anur2023@users.noreply.github.com>
2024-11-05 00:47:56 +0530

    Time: 0 ms (100%), Space: 39.9 MB (89.43%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,8 +1,17 @@
 class Solution {
     public int fib(int n) {
-        if (n<=1){
-            return n;
+        // By Using the dynamic programming
+        int [] memo= new int[n+1];
+        for(int i = 0;i<=n;i++){
+            memo[i] = -1;
         }
-        return fib(n-1)+fib(n-2);
+        return fibhelper(n,memo);
+        
     }
+    private int fibhelper(int n, int[] memo){
+        if (n<=1) return n;
+        if (memo[n] != -1) return memo[n];
+        memo[n] = fibhelper(n-1,memo) + fibhelper(n-2,memo);
+        return memo[n];
+     }
 }
