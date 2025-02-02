fcf108e7112d135a21ff4a72f50f329d395d5240
ANURUDDH KUMAR <154400979+anur2023@users.noreply.github.com>
2025-02-02 21:04:46 +0530

    Time: 0 ms (100%), Space: 41.9 MB (5.63%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,32 +1,15 @@
 class Solution {
-    public boolean check(int[] nums) {
-        if(nums.length==0)return true;
-        int min = nums[0];
-        int k = 0;
-        for(int i  = 1;i<nums.length;i++){
-            if(nums[i] < min ){
-                min = nums[i];
-                k = i;
+    public boolean check(int[] arr) {
+       int count=0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i]<arr[i-1]){
+                count++;
             }
         }
-        nums = reverse(nums,0,k-1);
-        nums = reverse(nums,k,nums.length-1);
-        nums = reverse(nums,0,nums.length-1);
-        for(int i = 1;i<nums.length;i++){
-            if(nums[i]<nums[i-1]) return false;
+        if(arr[arr.length-1]>arr[0]){
+            count++;
         }
-        return true;
-
-
-    }
-    private int [] reverse(int [] arr,int start,int end){
-        while(start<=end){
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end]  = temp;
-            start++;
-            end--;
-        }
-        return arr;
+        return count<=1;
+        
     }
 }
