74f411be6efeb336acff5caaf37ca34c214a570b
ANURUDDH KUMAR <154400979+anur2023@users.noreply.github.com>
2026-06-16 23:30:50 +0530

    Time: 1 ms (96.16%), Space: 46.6 MB (79.78%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -1,37 +1,39 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+import java.util.*;
+
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
+
         List<List<Integer>> ans = new ArrayList<>();
-        helper(root, 0, ans);
+
+        if (root == null) {
+            return ans;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            List<Integer> level = new ArrayList<>();
+
+            for (int i = 0; i < size; i++) {
+
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+
+            ans.add(level);
+        }
+
         return ans;
     }
-
-    void helper(TreeNode root, int level, List<List<Integer>> ans){
-        if (root == null) return;
-        //add new array if not present
-        if (level == ans.size()){
-            ans.add(new ArrayList<>());
-        }
-        //add curr val
-        ans.get(level).add(root.val);
-
-        helper(root.left, level + 1 , ans);
-        helper(root.right, level+1 , ans);
-
-    }
-    
 }
